8487fe5ba6f7ffe64c5603b7ecf57bf582620dcb
vesnaguja <80161546+vesnaguja@users.noreply.github.com>
2021-09-02 20:11:56 +0200

    delete unused imports

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,26 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useState, useEffect } from "react";
+
 import { Header } from "./sharedComponents/Header";
 import { MainPage } from "../app/MainPage/MainPage";
-
-import { getCharacters } from "../services/heroService";
 
 import "./App.css";
 
 function App() {
-  const [characters, setCharacters] = useState([]);
-
-  useEffect(() => {
-    getCharacters()
-      .then((data) => {
-        setCharacters(data);
-        return data;
-      })
-      .then((data) => {
-        console.log(data);
-      });
-  }, []);
-
+ 
   return (
     <>
       <Header />
--- src/app/MainPage/MyTeamSection/HeroListItem.jsx
+++ src/app/MainPage/MyTeamSection/HeroListItem.jsx
@@ -3,7 +3,7 @@ import { ListGroup } from "react-bootstrap";
 export const HeroListItem = () => {
   return (
     <ListGroup.Item className="d-flex row p-0 ms-2 my-2 border-3 border-secondary">
-      <img src="/images/ph.jpg" className="col-2  border-end border-3 border-secondary"/>
+      <img src="/images/ph.jpg" alt="" className="col-2  border-end border-3 border-secondary"/>
       <h5 className="col-10 d-flex mb-0 align-items-center">Some Hero</h5>
     </ListGroup.Item>
   );
